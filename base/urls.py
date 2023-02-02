304fd364dd72a95a7d134b2ff646d9560df7bd1c
apella1 <johncollons5@gmail.com>
2023-02-02 16:49:21 +0300

    Add dynamic routing in room template

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+# adding dynamic routing to the room view
+
 urlpatterns = [
     path('', views.home, name='home'),
-    path('room/', views.room, name='room'),
+    path('room/<str:pk>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
-# using function based views 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id':1, 'name': 'Open Source Contribution Guidelines'},
+    {'id':2, 'name': 'Fullstack JavaScript'},
+    {'id':3, 'name': 'Pythonistas Assemble'},
+    {'id':4, 'name': 'Gophers Dynasty'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+# using function based views 
+
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i 
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
